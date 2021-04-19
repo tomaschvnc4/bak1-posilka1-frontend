0233a45641326a6f6aa181abfbb4e4d8d7d451b8
tomaschvnc4 <tomaschvnc4@gmail.com>
2021-04-19 20:59:59 +0200

    zas popis

--- src/components/metaDecorator/MetaDecorator.jsx
+++ src/components/metaDecorator/MetaDecorator.jsx
@@ -5,15 +5,12 @@ import PropTypes from 'prop-types';
 const MetaDecorator = ({ title, description }) => {
    return (
       <Helmet>
-         {/* <meta charSet='utf-8' /> */}
+         <meta charSet='utf-8' />
          <title>{title}</title>
          <meta name='description' content={description} data-react-helmet='true'></meta>
-         <meta data-react-helmet='true' property='og:description' content='test' />
-         <meta
-            data-react-helmet='true'
-            property='og:title'
-            content='Using the Effect Hook – React'
-         />
+         <meta property='og:description' content='test' data-react-helmet='true' />
+         <meta data-react-helmet='true' property='og:title' content={title} />
+         <meta property='og: description' content={description} data-react-helmet='true' />
       </Helmet>
    );
 };
