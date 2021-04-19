dbe523db0bc7ce6381cd53543208a4b040c05def
tomaschvnc4 <tomaschvnc4@gmail.com>
2021-04-19 21:25:00 +0200

    zas popis

--- src/components/metaDecorator/MetaDecorator.jsx
+++ src/components/metaDecorator/MetaDecorator.jsx
@@ -9,10 +9,11 @@ const MetaDecorator = ({ metaData }) => {
          <meta charSet='utf-8' />
          <title>{title}</title>
          <meta name='description' content={description} data-react-helmet='true'></meta>
-
-         <meta data-react-helmet='true' property='og:title' content={title} />
-         <meta property='og: description' content={description} data-react-helmet='true' />
          <link rel='canonical' href={canonical} data-react-helmet='true' />
+
+         <meta property='og:title' content={title} data-react-helmet='true' />
+         <meta property='og:description' content={description} data-react-helmet='true' />
+         <meta property='og:url' content={canonical} data-react-helmet='true' />
       </Helmet>
    );
 };
